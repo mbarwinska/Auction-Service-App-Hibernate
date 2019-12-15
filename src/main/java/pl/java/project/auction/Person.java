cc4cd02b513bbbabb82e1af2e5f481d08998f447
Maja Barwińska <majakempa1@gmail.com>
2019-12-15 09:10:18 +0100

    Merge branch 'master' of C:\Users\Maja\Desktop\Java\JDBC\orm-sda with conflicts.

--- src/main/java/pl/java/project/auction/Person.java
+++ src/main/java/pl/java/project/auction/Person.java
@@ -1,20 +1,24 @@
 package pl.java.project.auction;
 
+
+
+
 import javax.persistence.Embeddable;
+import java.util.List;
 import java.util.Objects;
 
 @Embeddable
 public class Person {
 
+    private String name;
+    private String fullName;
+//    private List<Address> addresses;
+
+
     public Person(String name, String fullName) {
         this.name = name;
         this.fullName = fullName;
     }
-
-    private String name;
-    private String fullName;
-//    private List<Address> address;
-
 
     public String getName() {
         return name;
@@ -35,7 +39,7 @@ public class Person {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Person)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
         return Objects.equals(name, person.name) &&
                 Objects.equals(fullName, person.fullName);
