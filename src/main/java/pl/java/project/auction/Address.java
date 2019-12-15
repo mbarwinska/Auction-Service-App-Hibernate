64384a4c05b2adeae38dbeb88665b8fb45191f96
Krzysztof Kochel <krzysztof.kochel@protonmail.com>
2019-12-15 09:49:36 +0100

    Add Description type

--- src/main/java/pl/java/project/auction/Address.java
+++ src/main/java/pl/java/project/auction/Address.java
@@ -1,4 +1,73 @@
 package pl.java.project.auction;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Address {
+    @Id
+    private Long Id;
+    private String street;
+    private String city;
+    private int zipCode;
+    private String type;
+
+    protected Address () {}
+
+    public Address(String street, String city, int zipCode, String type) {
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address)) return false;
+        Address address = (Address) o;
+        return zipCode == address.zipCode &&
+                Id.equals(address.Id) &&
+                street.equals(address.street) &&
+                city.equals(address.city) &&
+                type.equals(address.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, street, city, zipCode, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "Id=" + Id +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", zipCode=" + zipCode +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
 }
