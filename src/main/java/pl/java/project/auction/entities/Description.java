aa759394a9f36eedfa117654510c0ad5ac547bc1
Krzysztof Kochel <krzysztof.kochel@protonmail.com>
2020-01-11 10:14:59 +0100

    modified bid & address entity

--- src/main/java/pl/java/project/auction/entities/Description.java
+++ src/main/java/pl/java/project/auction/entities/Description.java
@@ -1,25 +1,23 @@
 package pl.java.project.auction.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "OPIS")
 public class Description {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private String Content;
+    @Column(name = "zawartość")
+    private String content;
 
     protected Description() {
     }
 
     public Description(String content) {
-        Content = content;
+        this.content = content;
     }
 
     public Long getId() {
@@ -27,7 +25,7 @@ public class Description {
     }
 
     public String getContent() {
-        return Content;
+        return content;
     }
 
     @Override
@@ -36,19 +34,19 @@ public class Description {
         if (!(o instanceof Description)) return false;
         Description that = (Description) o;
         return id.equals(that.id) &&
-                Content.equals(that.Content);
+                content.equals(that.content);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, Content);
+        return Objects.hash(id, content);
     }
 
     @Override
     public String toString() {
         return "Description{" +
                 "id=" + id +
-                ", Content='" + Content + '\'' +
+                ", Content='" + content + '\'' +
                 '}';
     }
 }
